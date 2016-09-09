b385983622a776243f34bae950c8376ad3c84d8c
TianRunyan <tianrunyan@TianRunyandeMacBook-Pro.local>
2016-09-09 23:33:18 +0800

    初始项目

---
+++ SevenServer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SevenServer
+//
+//  Created by tianrunyan on 16/9/8.
+//  Copyright © 2016年 tianrunyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
